74ab2ad8899f52b548256d0376847dcc8bc19927
Rithik Agarwal <rithik.agarwal@honeywell.com>
2022-05-22 16:30:00 +0530

    fix

--- push.js
+++ push.js
@@ -5,10 +5,12 @@ let idKeys = {
   privateKey: '2M1oK4r2CkY3YEKUsafFh1MkosDtwVFDZuUffd15ZRQ'
 };
 
-push.setVapidDetails('mailto:ragarwal0201@gmail.com', idKeys.publicKey , idKeys.privateKey);
+push.setVapidDetails('mailto:mail@eg.com', idKeys.publicKey , idKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/f0-7oV895wE:APA91bFAHPF3xnWng1j2p1psJByeOnt24zJS4q5YDmXcC2P8YVuluYgONfJw87bsAVkDUoVA9gimKH68SBgleh7SoACCqAxmIedS8tzhsA_3EryoCIlFhvehN3UxvwOkmliefQ4ZBiPN","expirationTime":null,
-            "keys":{"p256dh":"BF3xb_9PBeAyyBK9hQlb68XygDD-pU8shFdZwemzdhmgZC58LEMO9fno6qGxqQUlRbgO8928JT8TNYa_1V-KsS4",
-                    "auth":"1YQKoQQYsXiv1-zNkzz2SQ"}};
-
+let sub = {
+        "endpoint":"https://fcm.googleapis.com/fcm/send/fJBViIebidI:APA91bHWP7PC7gbHr-btSk60aowqGwdi9W6XKpemkryT6nsWp5VsB3hOJKrSEFabFhG8racfcWP0p6lysNjdFfrm8qKGdPd9znoT19TxI40cBjJGhVmv-S2tQBqbPPC7pdNOY_tBcz6i","expirationTime":null,"keys":{"p256dh":"BElByk9eGlKW3C6hOb2cgi9g02kH9p7-80vO4my1j1g3axrXx6a77XlT29r6Gb6TGfAsHdWpLrAo8vgdwjsipYY","auth":"5WCF5RNNoUf3y4pC9Ww68A"},
+               };
+let sub2 ={"endpoint":"https://fcm.googleapis.com/fcm/send/cem2SIZnowQ:APA91bF-tnK0bSCjfKSRLBmfn1zMNWB_Aw34dL8s4IX9ZW-l3RoUaVUfQxSKhpOuI9HII5Gmp5ymHu-RLhqlIJNcPbh9rBZwzpLN_eNZaDKIfKIiGAcoKpFHG0ixHgikIc1klEcTuCi-","expirationTime":null,"keys":{"p256dh":"BJFUrvzU9LaqyYFtbXRzbAA_B8UBarJ6Q1TRD1pBegKhy_kyFxQRTJS6m4_S0feovQW6JkMqJFHYZn_qj4xHiO0","auth":"WZm5ZAt1vIiWktxUbKCrbA"}
+};
 push.sendNotification(sub, 'test message');
+push.sendNotification(sub2, 'test message');
--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 self.addEventListener('push', function(e) {
     var options = {
-      body: 'This notification was generated from a push!',
-      icon: 'images/example.png',
+      body: 'This notification was generated from demo website!',
+      icon: 'Honeywell-Emblem.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
         primaryKey: '2'
       },
       actions: [
-        {action: 'explore', title: 'Explore this new world',
+        {action: 'Explore', title: 'Visit the website',
           icon: 'images/checkmark.png'},
         {action: 'close', title: 'Close',
           icon: 'images/xmark.png'},
       ]
     };
     e.waitUntil(
-      self.registration.showNotification('Hello world!', options)
+      self.registration.showNotification('Change Alert!', options)
     );
   });
