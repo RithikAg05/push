19e7ec5ff90697e4ff88c2d84faab7440785e0f8
Rithik Agarwal <rithik.agarwal@honeywell.com>
2022-06-26 16:18:36 +0530

    change service worker

--- sw.js
+++ sw.js
@@ -1,20 +1,25 @@
 self.addEventListener('push', function(e) {
     var options = {
       body: 'This notification was generated from demo website!',
-      icon: 'Honeywell-Emblem.png',
+      icon: 'images\Honeywell-Emblem.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
         primaryKey: '2'
-      },
-      actions: [
-        {action: 'Explore', title: 'Visit the website',
-          icon: 'images/checkmark.png'},
-        {action: 'close', title: 'Close',
-          icon: 'images/xmark.png'},
-      ]
+      }
     };
     e.waitUntil(
       self.registration.showNotification('Change Alert!', options)
     );
   });
+
+  self.addEventListener('notificationclick', function(event) {
+  console.log('notification click recived .');
+
+event.notification.close();
+
+event.waitUntill(
+  clients.openWindow('https://www.honeywellforge.ai/')
+);
+  });
+
